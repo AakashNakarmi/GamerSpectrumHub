db99615ac4ad37891cd3694cd00e5bc8f7145858
AakashNakarmi <aakasnakarmee@gmail.com>
2023-11-02 23:14:53 +0530

    Built Game Cards and Game Grid

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Grid
-        templateAreas={{ base: `"nav main"`, lg: `"nav nav" "aside main"` }}
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
       >
         <GridItem area="nav">
           <Navbar />
